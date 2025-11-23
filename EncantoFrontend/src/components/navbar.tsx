1d65c66578715d800dc02aa12cffa0b6e954d6a8
DYTateno <diogo.tateno@sptech.school>
2025-11-23 13:39:49 -0300

    Add: iniciei o arquivo navbar, testando o style do componente

---
+++ EncantoFrontend/src/components/navbar.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import React from 'react';
+
+
+function Navbar() {
+
+    return (
+        <div style={{ backgroundColor: 'red', alignItems: 'center', width: '100vw', display: 'flex', position: 'fixed', justifyContent:'center', justifySelf: 'center', top: '0%', height: '48px' }}>
+            <div style={{ backgroundColor: 'blue' }}>:D</div>
+        </div>
+
+    )
+
+}
+
+export default Navbar
