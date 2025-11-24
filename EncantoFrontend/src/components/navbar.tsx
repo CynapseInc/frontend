138b7890ba6e638b6bcb09bed01f3738be0c2541
DYTateno <diogo.tateno@sptech.school>
2025-11-24 11:29:20 -0300

    Fix: adicionei padding nos botões carrinho e login

--- EncantoFrontend/src/components/navbar.tsx
+++ EncantoFrontend/src/components/navbar.tsx
@@ -8,24 +8,25 @@ function Navbar() {
     return (
         <div style={{ backgroundColor: 'white', alignItems: 'center', width: '100vw', display: 'flex', position: 'fixed', justifyContent: 'center', justifySelf: 'center', top: '0%', height: '56px', gap: "20px" }}>
 
-
+                {/* Logo Encanto Personalizados */}
             <div><img src={logo} alt="logoEncanto" height={"48px"} /></div>
 
+                {/* Centro da navbar  */}
             <div style={{ backgroundColor: 'blue', width: "50%", flexDirection:"row", display:"flex", gap:"5%", justifyContent:"center"}}>
 
+                {/* Opções da nav */}
                 <div style={{ fontSize: "80%" }}>Home</div>
                 <div style={{ fontSize: "80%" }}>Pedidos</div>
                 <div style={{ fontSize: "80%" }}>Financeiro</div>
-
                 <div style={{ fontSize: "80%" }}>Produtos</div>
-
                 <div style={{ fontSize: "80%" }}>Funcionários</div>
 
 
             </div>
 
-            <div style={{ backgroundColor: "green", width: "fit-content" }}>Login</div>
-            <div style={{ backgroundColor: "green", width: "fit-content" }}>Carrinho</div>
+                {/* Botão login e botão carrinho  */}
+            <div style={{ backgroundColor: "green", width: "fit-content", padding:"3px" }}>Login</div>
+            <div style={{ backgroundColor: "green", width: "fit-content", padding:"3px" }}>Carrinho</div>
 
         </div>
 
