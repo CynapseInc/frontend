3cf3aa0c73564e1106a32b820d9620f50716eaf3
DYTateno <diogo.tateno@sptech.school>
2025-12-02 17:13:58 -0300

    feat: adicionei seções que não existiam (botão, input)
    fix: deixei as proporções mais fiel ao protótipo

--- EncantoFrontend/src/components/login/login.tsx
+++ EncantoFrontend/src/components/login/login.tsx
@@ -1,16 +1,44 @@
 import styles from './login.module.css';
 
-export default function Login(){
+export default function Login() {
 
-     return (
+  return (
     <div className={styles.conteudo}>
       <div className={styles.CardMain}>
         <div className={styles.DecorationTop}>
-            <div className={styles.icon}>goku</div>
-            <img src="./src/assets/logoEncanto.png" alt="" />
-            <div className={styles.subtitle}>picollo</div>
-            <div className={styles.subtitle}>vegeta</div>
+          <div className={styles.icon}>goku</div>
+          <img src="./src/assets/logoEncanto.png" alt="" />
+          <div className={styles.subtitle}><h3>Área do Administrador</h3></div>
+          <div className={styles.subtitle}>Entre com suas credenciais</div>
         </div>
+        <div className={styles.inputArea}>
+
+          E-mail
+          <div className={styles.inputCase}>
+            
+          <input type="text" />
+                    <input type="text" />
+
+          <input type="text" />
+
+          
+          
+          </div>
+
+          Senha
+           <div className={styles.inputCase}>
+          <input type="text" />
+          </div>
+
+        </div>
+
+        <div className={styles.inputMisc}></div>
+        <div className={styles.buttonLogin}>Entrar</div>
+
+        <div className={styles.aviso}>
+          Acesso exclusivo para administradores
+        </div>
+
       </div>
     </div>
   );
