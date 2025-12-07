f02e0cc69bef96090de83bc67cf4af7183c8d476
DYTateno <diogo.tateno@sptech.school>
2025-12-06 21:19:43 -0300

    feat: aparência atualizada da navbar, mais similar ao protótipo

--- EncantoFrontend/src/components/navbar.tsx
+++ EncantoFrontend/src/components/navbar.tsx
@@ -1,32 +1,34 @@
 import { useState } from 'react'
 import React from 'react';
+import { FaCircleUser } from "react-icons/fa6";
+import { PiShoppingCartSimple } from "react-icons/pi";
 import logo from '../assets/logoEncanto.png';
 
 
 function Navbar() {
 
     return (
-        <div style={{ backgroundColor: 'white', alignItems: 'center', width: '100vw', display: 'flex', position: 'fixed', justifyContent: 'center', justifySelf: 'center', top: '0%', height: '56px', gap: "20px", zIndex: "10" }}>
+        <div style={{ backgroundColor: 'white', alignItems: 'center', width: '100vw', display: 'flex', position: 'fixed', justifyContent: 'center', justifySelf: 'center', top: '0%', height: '70px', gap: "40px", zIndex: "10", filter: "drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.15))" }}>
 
-                {/* Logo Encanto Personalizados */}
-            <div><img src={logo} alt="logoEncanto" height={"48px"} /></div>
+            {/* Logo Encanto Personalizados */}
+            <div><img src={logo} alt="logoEncanto" height={"56px"} /></div>
 
-                {/* Centro da navbar  */}
-            <div style={{ backgroundColor: 'blue', width: "50%", flexDirection:"row", display:"flex", gap:"5%", justifyContent:"center"}}>
+            {/* Centro da navbar  */}
+            <div style={{ width: "50%", flexDirection: "row", display: "flex", gap: "5%", justifyContent: "center" }}>
 
                 {/* Opções da nav */}
-                <div style={{ fontSize: "80%" }}>Home</div>
-                <div style={{ fontSize: "80%" }}>Pedidos</div>
-                <div style={{ fontSize: "80%" }}>Financeiro</div>
-                <div style={{ fontSize: "80%" }}>Produtos</div>
-                <div style={{ fontSize: "80%" }}>Funcionários</div>
+                <div style={{ fontSize: "100%" }}>Home</div>
+                <div style={{ fontSize: "100%" }}>Pedidos</div>
+                <div style={{ fontSize: "100%" }}>Financeiro</div>
+                <div style={{ fontSize: "100%" }}>Produtos</div>
+                <div style={{ fontSize: "100%" }}>Funcionários</div>
 
 
             </div>
 
-                {/* Botão login e botão carrinho  */}
-            <div style={{ backgroundColor: "green", width: "fit-content", padding:"3px" }}>Login</div>
-            <div style={{ backgroundColor: "green", width: "fit-content", padding:"3px" }}>Carrinho</div>
+            {/* Botão login e botão carrinho  */}
+            <div style={{ color: "#f4acacff", display:"flex", width: "fit-content", height: "fit-content", padding: "3px", scale: "2.5" }}><FaCircleUser /></div>
+            <div style={{ backgroundColor: "#fbc9bbff", color:"grey", display:"flex", width: "fit-content", height: "fit-content", padding: "5px", scale: "1.6", borderRadius:"8px" }}><PiShoppingCartSimple /></div>
 
         </div>
 
